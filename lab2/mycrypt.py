e6614d51d53d777e080cff1841356f269ef39d06
Tuomas Määttä <48020687+rekk5@users.noreply.github.com>
2024-03-11 01:51:35 +0200

    issues0, 7-9

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,24 +1,46 @@
 import codecs
 
+digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+
 def encode(s):
+    originlen = len(s)
     if not isinstance(s,str):
         raise TypeError
-    origlen = len(s)
-    crypted = ""
-    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
-    if len(s) > 1000:
+    elif originlen > 1000:
         raise ValueError
-    for c in s:
-        if c.isalpha():
-            if c.islower():
-                c=c.upper()
-            # Rot13 the character for maximum security
-            crypted+=codecs.encode(c,'rot13')
-        elif c in digitmapping:
-          crypted+=digitmapping[c]
+    
 
-    return crypted
+    encrypted = ""
+    sjusted = s.ljust(1001, "a")
+    for c in sjusted:
+        if c in "+åäöÅÄÖ":
+            raise ValueError
+        elif c.isalpha():
+            # Rot13 the character for maximum security
+            encrypted += codecs.encode(c,'rot13')
+        elif c in digitmapping:
+            encrypted += digitmapping[c]
+        else:
+            raise ValueError
+        
+    return encrypted[:originlen].upper()
 
 def decode(s):
-    return s
-
+    originlen = len(s)
+    if not isinstance(s,str):
+        raise TypeError
+    elif originlen > 1000:
+        raise ValueError
+    decrypted = ""
+    sjusted = s.ljust(1001, "a")
+    for c in sjusted:
+        if c in "+åäöÅÄÖ":
+            raise ValueError
+        elif c.isalpha():
+            # Rot13 the character for maximum security
+            decrypted += codecs.encode(c,'rot13')
+        elif c in digitmapping:
+            decrypted += digitmapping[c]
+        else:
+            raise ValueError
+    return decrypted[:originlen].lower()
