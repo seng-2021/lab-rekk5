6defe26942d018d63ab38125a14513038757218f
Tuomas Määttä <48020687+rekk5@users.noreply.github.com>
2024-03-11 09:22:43 +0200

    Done know

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -9,10 +9,8 @@ def encode(s):
     if len(s) > 1000:
         raise ValueError
     s = s.ljust(1000, 'a')
-    
-    
     for c in s:
-         if c.lower() in ["+", "å", "ä", "ö"]:
+        if c.lower() in ["+", "å", "ä", "ö"]:
             raise ValueError
     for c in s:
         if c.isalpha():
@@ -30,17 +28,17 @@ def encode(s):
     return crypted[:originlen]
 
 def decode(s):
-
-
+    if not isinstance(s,str):
+        raise TypeError
     originlen = len(s)
     crypted = ""
     digitmapping = dict(zip('!"#€%&/()=1234567890','1234567890!"#€%&/()='))
-    
-    
+    if len(s) > 1000:
+        raise ValueError
     s = s.ljust(1000, 'a')
-    
-    
-    
+    for c in s:
+        if c.lower() in ["+", "å", "ä", "ö"]:
+            raise ValueError
     for c in s:
         if c.isalpha():
             if c.islower():
