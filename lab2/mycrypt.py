b48e2b6c4e995dd7b4d99a8e1d783ada5cd9eb7b
Tuomas Määttä <48020687+rekk5@users.noreply.github.com>
2024-03-11 08:30:11 +0200

    Issue10

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,46 +1,43 @@
 import codecs
 
-digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
-
 def encode(s):
-    originlen = len(s)
     if not isinstance(s,str):
         raise TypeError
-    elif originlen > 1000:
+    originlen = len(s)
+    crypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
         raise ValueError
-    
-
-    encrypted = ""
-    sjusted = s.ljust(1001, "a")
-    for c in sjusted:
-        if c in "+åäöÅÄÖ":
+    s = s.ljust(1000, 'a')
+    for c in s:
+         if c.lower() in ["+", "å", "ä", "ö"]:
             raise ValueError
-        elif c.isalpha():
+    for c in s:
+        if c.isalpha():
+            if c.islower():
+                c=c.upper()
             # Rot13 the character for maximum security
-            encrypted += codecs.encode(c,'rot13')
+            elif c.isupper():
+                 c=c.lower()
+            crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
-            encrypted += digitmapping[c]
-        else:
-            raise ValueError
-        
-    return encrypted[:originlen].upper()
+          crypted+=digitmapping[c]
+    return crypted[:origlen]
 
 def decode(s):
     originlen = len(s)
-    if not isinstance(s,str):
-        raise TypeError
-    elif originlen > 1000:
-        raise ValueError
-    decrypted = ""
-    sjusted = s.ljust(1001, "a")
-    for c in sjusted:
-        if c in "+åäöÅÄÖ":
-            raise ValueError
-        elif c.isalpha():
+    crypted = ""
+    digitmapping = dict(zip('!"#€%&/()=1234567890','1234567890!"#€%&/()='))
+    s = s.ljust(1000, 'a')
+    for c in s:
+        if c.isalpha():
+            if c.islower():
+                c=c.upper()
             # Rot13 the character for maximum security
-            decrypted += codecs.encode(c,'rot13')
+            elif c.isupper():
+                 c=c.lower()
+            crypted+=codecs.decode(c,'rot13')
         elif c in digitmapping:
-            decrypted += digitmapping[c]
-        else:
-            raise ValueError
-    return decrypted[:originlen].lower()
+          crypted+=digitmapping[c]
+
+    return crypted[:origlen]
